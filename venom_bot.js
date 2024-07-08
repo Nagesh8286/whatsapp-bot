b4c6c2bf63921e88c719cef7647d04282ce3aa3c
Nagesh8286 <mis@ramanexpo.com>
2024-07-08 17:06:00 +0530

    venom_bot.js

--- venom_bot.js
+++ venom_bot.js
@@ -339,3 +339,12 @@ async function sendFilesInParallel(client, to, links) {
 async function sendFile(client, to, link) {
   await sendFilesInParallel(client, to, [link]);
 }
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, Render!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
